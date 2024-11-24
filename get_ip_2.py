2eb5341d1f2e132db74f4027ffb3b539aefde78b
magetel <magonzalez@teleconline.com>
2024-11-24 18:16:42 +0100

    fecha2

--- get_ip_2.py
+++ get_ip_2.py
@@ -7,6 +7,6 @@ loc = get('https://ipapi.co/json/')
 st.write(loc.json())
 datos = 1
 
-fecha = str(datetime.datetime.now())
+fecha = datetime.datetime.now().strftime('%d/%m/%Y_%H_%M_%S')
 with open('json_data' +fecha+'.json', 'w') as outfile:
     json.dump(loc.json(),outfile)

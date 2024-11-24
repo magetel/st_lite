0271e7aae90761d5788e1ce4861282ff5614ad8c
MIGUEL A González <magonzalez@teleconline.com>
2024-11-24 18:05:53 +0100

    fecha

--- get_ip_2.py
+++ get_ip_2.py
@@ -1,9 +1,12 @@
 import streamlit as st 
 from requests import get
 import json
+import datetime
 
 loc = get('https://ipapi.co/json/')
 st.write(loc.json())
+datos = 1
 
-with open('json_data.json', 'w') as outfile:
+fecha = str(datetime.datetime.now())
+with open('json_data' +fecha+'.json', 'w') as outfile:
     json.dump(loc.json(),outfile)
